8359ff83706865f7eb2a17f0a439712806795865
Safiraachoui <adilasafira213@gmail.com>
2024-01-02 22:34:53 +0700

    tugas google collab

---
+++ output-nama.py
@@ -0,0 +1,7 @@
+first_name = "John"
+last_name = "Doe"
+
+# ingin output menjadi "John Doe"
+print(first_name+last_name) # tanpa spasi
+print(first_name+" "+last_name) # menggunakan spasi
+print("Hello" , first_name+" "+last_name)
